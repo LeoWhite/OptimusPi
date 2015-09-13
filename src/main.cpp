6f396b78d197970ef4f59dd91c433339ca3b8803
Leo White <leo.white@mybigideas.co.uk>
2015-09-13 18:37:51 +0100

    Initial CMakeLists.txt file and HelloWorld file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+#include "OptimusPiConfig.h"
+
+int main(int argc,char *argv[]) {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Version " << OptimusPi_VERSION_MAJOR << "." << OptimusPi_VERSION_MINOR << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
