b8a6174446e070da0db3e3255c9d3f75a5cde5c5
Leo White <leo.white@mybigideas.co.uk>
2015-11-03 13:58:11 +0000

    Added a RTIMU sensor class. This makes use of the RTIMU library to extract information from the sensors on the SenseHAT mounted on the robot

---
+++ src/PiWars/SensorRTIMU.cpp
@@ -0,0 +1,113 @@
+#include "SensorRTIMU.h"
+#include "RTIMULib.h"
+
+#include <iostream>
+#include <thread>
+
+namespace PiWars
+{
+
+  
+SensorRTIMU::SensorRTIMU() 
+  : Sensor()
+  , _initialised(false)
+  , _pitch(0)
+  , _roll(0)
+  , _yaw(0)
+  , _rtimuReader(nullptr)
+  , _rtimuReaderQuit(false) 
+{
+}
+
+SensorRTIMU::~SensorRTIMU() {
+}
+
+bool SensorRTIMU::exists() {
+  // Always return true for now, as the RTIMU library supports various
+  // sensors
+  return true;
+}
+
+bool SensorRTIMU::enable() {
+  if(!isEnabled()) {
+    // Create a thread to poll the range sensor, so there is always a valid
+    // range ready to be read.
+    _rtimuReaderQuit = false;
+    _rtimuReader = new std::thread(rtimuReader, std::ref(_rtimuReaderQuit), std::ref(_pitch), std::ref(_roll), std::ref(_yaw));
+    
+    // Call the base class to perform any 
+    // generic changes
+    Sensor::enable();
+  }
+  
+  return isEnabled();
+    
+}
+
+void SensorRTIMU::disable() {
+  if(isEnabled()) {
+    // Tell the thread to exit
+    _rtimuReaderQuit = false;
+    
+    // and wait for it to do so
+    _rtimuReader->join();
+    _rtimuReader = nullptr;    
+    
+    Sensor::disable();
+  }
+}
+void SensorRTIMU::fusion(float &pitch, float &roll, float &yaw) {
+  pitch = _pitch;
+  roll = _roll;
+  yaw = _yaw;
+}
+
+void SensorRTIMU::rtimuReader(std::atomic<bool> &quit, std::atomic<float> &pitch, std::atomic<float> &roll, std::atomic<float> &yaw) 
+{
+  // read in the main settings
+  RTIMUSettings *settings = new RTIMUSettings("/etc", "RTIMULib");
+  
+  RTIMU *imu = RTIMU::createIMU(settings);
+
+  if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL)) {
+    std::cerr << "IMU not found!" << std::endl;
+      
+    // Exit out early
+    return;
+  }
+  
+  imu->IMUInit();
+
+  //  this is a convenient place to change fusion parameters
+
+  imu->setSlerpPower(0.02);
+  imu->setGyroEnable(true);
+  imu->setAccelEnable(true);
+  imu->setCompassEnable(true);
+
+  
+  while(!quit.load()) {
+  
+    std::this_thread::sleep_for (std::chrono::milliseconds(imu->IMUGetPollInterval()));
+      
+    if(imu->IMURead()) {
+      RTIMU_DATA imuData = imu->getIMUData();
+
+      if(imuData.fusionPoseValid) {
+        pitch = imuData.fusionPose.x()* RTMATH_RAD_TO_DEGREE;
+        roll = imuData.fusionPose.y()* RTMATH_RAD_TO_DEGREE;
+        yaw = imuData.fusionPose.z() * RTMATH_RAD_TO_DEGREE;
+      }
+    }
+  }
+  
+  delete imu;
+  delete settings;
+}
+
+void SensorRTIMU::init() {
+  // Nothing to be done for now
+  
+}
+
+}
---
+++ src/PiWars/SensorRTIMU.h
@@ -0,0 +1,62 @@
+/**
+ */
+
+#ifndef _PIWARS_SENSORRTIMU_H
+#define _PIWARS_SENSORRTIMU_H
+
+#include <atomic>
+#include <thread>
+#include <cstdint>
+#include <cstddef>
+
+#include "Sensor.h"
+#include "I2C.h"
+
+namespace PiWars {
+
+class SensorRTIMU : public Sensor {
+  public:
+    // Initialize the RTIMU ready for use
+    SensorRTIMU();
+    ~SensorRTIMU();
+
+    // Checks if the sensor is present. Some sensors can be dynamically
+    // added or removed from the robot, or turned on and off, so arn't
+    // always available for use.
+    //
+    // @returns true if the sensor exists
+    bool exists();
+
+    // Enables the sensor ready for use.
+    // Some sensors use additional power or CPU resources
+    // when running, so we only want to enable them when
+    // we're ready to use them
+    // @returns true if successfully enabled
+    bool enable();
+
+    // Disable a sensor, potentially reducing power or CPU usage
+    void disable();
+   
+    // Returns the current values
+    //
+    // @param pitch Filled in with the current pitch
+    // @param roll  Filled in with the current roll
+    // @param yaw   Filled in wit the current yaw
+    void fusion(float &pitch, float &roll, float &yaw);
+
+  private:
+    void init(); //<! Initialise the range sensor
+    static void rtimuReader(std::atomic<bool> &quit, std::atomic<float> &pitch, std::atomic<float> &roll, std::atomic<float> &yaw); //<! Background thread for polling the sensor
+
+    bool _initialised; //<! Indicates if the sensor has been intialised
+    std::atomic<float> _pitch; //<! The last successfully read in pitch.
+    std::atomic<float> _roll; //<! The last successfully read in roll.
+    std::atomic<float> _yaw; //<! The last successfully read in yaw.
+      
+    std::thread *_rtimuReader; //<! Background thread for reading in the values
+    std::atomic<bool> _rtimuReaderQuit; //<! Used to indicate when the thread should exit
+};
+
+}
+
+#endif
